a9d9e08a97a27d22e3d20df186d8fa88ffe76f5b
Arco Gelderblom <arco.gelderblom@ziggo.nl>
2017-12-18 17:13:46 +0100

    Excercise 5.9A

---
+++ Week_4/5.9A.py
@@ -0,0 +1,25 @@
+import os, gzip, pickle, numpy as np
+
+from urllib import request
+from matplotlib import cm
+from matplotlib.pyplot import imshow, show
+
+url = "http://deeplearning.net/data/mnist/mnist.pkl.gz"
+if not os.path.isfile("Dataset/mnist.pkl.gz"):
+    request.urlretrieve(url, "Dataset/mnist.pkl.gz")
+
+f = gzip.open('Dataset/mnist.pkl.gz', 'rb')
+train_set , valid_set , test_set = pickle.load(f, encoding='latin1')
+f.close()
+
+
+def get_image(number):
+    (X, y) = [img[number] for img in train_set]
+    return (np.array(X), y)
+
+
+def view_image(number):
+    (X, y) = get_image(number)
+    print("Label: %s" % y)
+    imshow(X.reshape(28,28), cmap=cm.gray)
+    show()
