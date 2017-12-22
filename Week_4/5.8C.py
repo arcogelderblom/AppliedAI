Fix forward function, correctly adds bias. Does not yet work with backprop of Huib Aldewereld

Week_4/5.8C.py
@@ -24,12 +24,15 @@ def forward(inputs,weights,function=sigmoid,step=-1):
     step=0 returns the inputs
     any step in between, returns the output vector of that particular (hidden) layer"""
     if step == 0:
-        return inputs
+        return np.append(1, inputs)
     elif step == -1:
         step = len(weights)
         return sigmoid(np.dot(weights[step-1], forward(inputs, weights, sigmoid, step-1)))
     else:
-        return sigmoid(np.dot(weights[step-1], forward(inputs, weights, sigmoid, step-1)))
+        if step == len(weights):
+            return sigmoid(np.dot(weights[step-1], forward(inputs, weights, sigmoid, step-1)))
+        else:
+            return np.append(1, sigmoid(np.dot(weights[step-1], forward(inputs, weights, sigmoid, step-1))))
 
 
 def backprop(inputs, outputs, weights, function=sigmoid, derivative=derivative_sigmoid, eta=0.01):
@@ -67,10 +70,19 @@ def backprop(inputs, outputs, weights, function=sigmoid, derivative=derivative_s
 # for index in range(len(w)):
 #     w[index] = w[index] + deltas[index]
 
-data = [[[1, 0, 0], 1],
-     [[1, 0, 1], 0],
-     [[1, 1, 0], 0],
-     [[1, 1, 1], 0]]
+inputs = [np.array([0, 0]),
+        np.array([0, 1]),
+        np.array([1, 0]),
+        np.array([1, 1])]
 
-w = np.array([[1,0.5,0.5]])
+output = [np.array([1]),
+          np.array([0]),
+          np.array([0]),
+          np.array([0])]
 
+w = [np.array([[1, 0.5, 0.5], [1, 0.5, 0.5]]), np.array([1, 0.5, 0.5])]
+#print(w)
+#print(inputs[0])
+#print(output[0])
+print(backprop(inputs[0], output[0], w))
+print(forward(inputs[0], w, step=0))